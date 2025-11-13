788809b76e81385c2a66ef4575818996aedf8269
linhthach988-hue <linhthach988@gmail.com>
2025-11-14 01:39:12 +0700

    update

--- src/services/studentsServices.js
+++ src/services/studentsServices.js
@@ -2,14 +2,14 @@ import axios from "axios";
 const endPoints = process.env.REACT_APP_API_BASE_URL;
 
 export async function list() {
-  const url = `${endPoints}/api/students`;
+  const url = `${endPoints}/students`;
   console.log(url);
   const response = await axios.get(url);
   return response.data.data;
 }
 
 export async function add(name, className, age) {
-  const url = `${endPoints}/api/students`;
+  const url = `${endPoints}/students`;
   console.log(url);
 
   return await axios.post(url, {
